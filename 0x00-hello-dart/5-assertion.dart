c0086e4f7432bcd029c9b5b694fecda2f0bed3df
KayKilb <115676011+KayKilb@users.noreply.github.com>
2024-10-07 20:11:27 -0500

    technically my code worked but it was not passing checker, redoing code to get it working on intranet

--- 0x00-hello-dart/5-assertion.dart
+++ 0x00-hello-dart/5-assertion.dart
@@ -1,5 +1,5 @@
-void main() {
-  var nb = 80;
-  assert(nb >= 80, 'The score must be bigger or equal to 80');
-  print('You Passed');
+void main(List<String> args) {
+  int score = int.parse(args[0]);
+  assert(score >= 80, '"The score must be bigger or equal to 80\n');
+  print('"You Passed"');
 }
