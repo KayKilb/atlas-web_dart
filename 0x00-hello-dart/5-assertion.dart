c09e14d8363c12f8551923b823a10bd74880e4ef
KayKilb <115676011+KayKilb@users.noreply.github.com>
2024-10-07 20:05:00 -0500

    no new line needed for this code

--- 0x00-hello-dart/5-assertion.dart
+++ 0x00-hello-dart/5-assertion.dart
@@ -1,5 +1,5 @@
 void main() {
   var nb = 80;
-  assert(nb >= 80, 'The score must be bigger or equal to 80\n');
+  assert(nb >= 80, 'The score must be bigger or equal to 80');
   print('You Passed');
 }
