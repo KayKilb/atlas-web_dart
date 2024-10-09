9218302896be03779d72581dcdf7b464428aee30
KayKilb <115676011+KayKilb@users.noreply.github.com>
2024-10-08 23:15:54 -0500

    inheritance file not working

--- 0x02-dart-classes/6-inheritance.dart
+++ 0x02-dart-classes/6-inheritance.dart
@@ -1,15 +1,31 @@
-class User extends Password {
+class User {
   int id;
   String name;
   int age;
   double height;
+  Password userPassword;
 
-  User(
-      {required this.id,
-      required this.name,
-      required this.age,
-      required this.height,
-      required String user_password})
+  User({required this.id, required this.name, required this.age, required this.height, required String user_password})
+      : userPassword = Password(password: user_password);
+
+  Map<String, dynamic> toJson() {
+    return {'id': id, 'name': name, 'age': age, 'height': height};
+  }
+
+  static User fromJson(Map<dynamic, dynamic> userJson) {
+    return User(
+        id: userJson['id'],
+        name: userJson['name'],
+        age: userJson['age'],
+        height: userJson['height'],
+        user_password: userJson['user_password']);
+  }
+
+  @override
+  String toString() {
+    return "User(id : \$id ,name: \$name, age: \$age, height: \$height, Password: \${userPassword.isValid()})";
+  }
+})
       : super(password: user_password);
 
   Map<String, dynamic> toJson() {
