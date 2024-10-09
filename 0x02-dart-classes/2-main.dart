d090d32b7e7892b0e79852ccd4e8958d8a81063a
KayKilb <115676011+KayKilb@users.noreply.github.com>
2024-10-08 23:46:51 -0500

    still working on 2

--- 0x02-dart-classes/2-main.dart
+++ 0x02-dart-classes/2-main.dart
@@ -2,15 +2,18 @@ import '2-password.dart';
 
 void main() {
   final ps = Password(password: "Just3z");
-  if (ps.isValid() == true)
-    print("\${ps.password} is a Valid Password");
-  else
-    print("\${ps.password} is Not a Valid Password");
-  print("\${ps.toString()}");
+  if (ps.isValid()) {
+    print("${ps.password} is a Valid Password");
+  } else {
+    print("${ps.password} is Not a Valid Password");
+  }
+  print(ps.toString());
+
   ps.password = "ShouldWorkf7ne";
-  if (ps.isValid() == true)
-    print("\${ps.password} is a Valid Password");
-  else
-    print("\${ps.password} is Not a Valid Password");
-  print("\${ps.toString()}");
+  if (ps.isValid()) {
+    print("${ps.password} is a Valid Password");
+  } else {
+    print("${ps.password} is Not a Valid Password");
+  }
+  print(ps.toString());
 }
