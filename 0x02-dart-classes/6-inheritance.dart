67146c4dd0bb892d199ebcd1b3f16a7d7aed6388
KayKilb <115676011+KayKilb@users.noreply.github.com>
2024-10-08 23:21:08 -0500

    made changes to file 6

--- 0x02-dart-classes/6-inheritance.dart
+++ 0x02-dart-classes/6-inheritance.dart
@@ -5,8 +5,13 @@ class User {
   double height;
   Password userPassword;
 
-  User({required this.id, required this.name, required this.age, required this.height, required String user_password})
-      : userPassword = Password(password: user_password);
+  User({
+    required this.id,
+    required this.name,
+    required this.age,
+    required this.height,
+    required String user_password,
+  }) : userPassword = Password(password: user_password);
 
   Map<String, dynamic> toJson() {
     return {'id': id, 'name': name, 'age': age, 'height': height};
@@ -14,35 +19,16 @@ class User {
 
   static User fromJson(Map<dynamic, dynamic> userJson) {
     return User(
-        id: userJson['id'],
-        name: userJson['name'],
-        age: userJson['age'],
-        height: userJson['height'],
-        user_password: userJson['user_password']);
+      id: userJson['id'],
+      name: userJson['name'],
+      age: userJson['age'],
+      height: userJson['height'],
+      user_password: userJson['user_password'],
+    );
   }
 
   @override
   String toString() {
-    return "User(id : \$id ,name: \$name, age: \$age, height: \$height, Password: \${userPassword.isValid()})";
-  }
-})
-      : super(password: user_password);
-
-  Map<String, dynamic> toJson() {
-    return {'id': id, 'name': name, 'age': age, 'height': height};
-  }
-
-  static User fromJson(Map<dynamic, dynamic> userJson) {
-    return User(
-        id: userJson['id'],
-        name: userJson['name'],
-        age: userJson['age'],
-        height: userJson['height'],
-        user_password: userJson['user_password']);
-  }
-
-  @override
-  String toString() {
-    return "User(id : $id ,name: $name, age: $age, height: $height, Password: ${isValid()})";
+    return "User(id : $id ,name: $name, age: $age, height: $height, Password: ${userPassword.isValid()})";
   }
 }
--- 0x02-dart-classes/6-main.dart
+++ 0x02-dart-classes/6-main.dart
@@ -1,29 +1,30 @@
-import '6-inheritance.dart';
-
 void main() {
   final djo = User(
-      id: 1, name: "Djo", age: 25, height: 1.89, user_password: "Azert23defde");
-  print('<===========Json=========>:');
-  print('\n');
+    id: 1,
+    name: "Djo",
+    age: 25,
+    height: 1.89,
+    user_password: "Azert23defde",
+  );
+  print('<===========Json=========>:\n');
   print(djo.toJson());
-  Map map = {
+
+  Map<String, dynamic> map = {
     'id': 3,
     'name': 'Youssef',
     'age': 26,
     'height': 1.9,
-    'user_password': "Azert23defde"
+    'user_password': "Azert23defde",
   };
   final youssef = User.fromJson(map);
-  print('\n');
-  print('<===========Test1===========>:');
-  print('\n');
+
+  print('\n<===========Test1===========>:\n');
   print(djo.toString());
   print(youssef.toString());
-  print('\n');
-  print('<===========Test2===========>:');
-  print('\n');
-  djo.user_password = "holberton98";
-  youssef.user_password = "AZERfghn6789";
+
+  print('\n<===========Test2===========>:\n');
+  djo.userPassword.password = "holberton98";
+  youssef.userPassword.password = "AZERfghn6789";
   print(djo.toString());
   print(youssef.toString());
 }
