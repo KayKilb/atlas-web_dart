bd7510197c08857e6d717c8c5af32f87cb2ceb6b
KayKilb <115676011+KayKilb@users.noreply.github.com>
2024-10-08 23:44:00 -0500

    added the missing password parameter in the Password

--- 0x02-dart-classes/2-main.dart
+++ 0x02-dart-classes/2-main.dart
@@ -1,17 +1,16 @@
 import '2-password.dart';
 
 void main() {
-  final ps = Password();
-  ps.password = "Just3z";
+  final ps = Password(password: "Just3z");
   if (ps.isValid() == true)
-    print("${ps.password} is a Valid Password");
+    print("\${ps.password} is a Valid Password");
   else
-    print("${ps.password} is Not a Valid Password");
-  print("${ps.toString()}");
+    print("\${ps.password} is Not a Valid Password");
+  print("\${ps.toString()}");
   ps.password = "ShouldWorkf7ne";
   if (ps.isValid() == true)
-    print("${ps.password} is a Valid Password");
+    print("\${ps.password} is a Valid Password");
   else
-    print("${ps.password} is Not a Valid Password");
-  print("${ps.toString()}");
+    print("\${ps.password} is Not a Valid Password");
+  print("\${ps.toString()}");
 }
