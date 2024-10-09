b414e2359c4abefe212b61f1b167cc508582428f
KayKilb <115676011+KayKilb@users.noreply.github.com>
2024-10-08 23:54:18 -0500

    updated the Password class to make the password property private and refactored the main function accordingly

--- 0x02-dart-classes/3-private_password.dart
+++ 0x02-dart-classes/3-private_password.dart
@@ -1,7 +1,10 @@
 class Password {
-  String _password;
+  String _password = '';
 
   Password({required String password}) : _password = password;
+
+  String get password => _password;
+  set password(String value) => _password = value;
 
   bool isValid() {
     return _password.length >= 8 &&
@@ -13,6 +16,16 @@ class Password {
 
   @override
   String toString() {
-    return "Your Password is $_password";
+    return 'Your Password is: $_password';
   }
+}
+
+void main() {
+  final ps = Password(password: "Passwordcode");
+  print(ps);
+  print(ps.isValid());
+
+  final ps2 = Password(password: "PasswordDecode3");
+  print(ps2);
+  print(ps2.isValid());
 }
