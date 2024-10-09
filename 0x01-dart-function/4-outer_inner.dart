ebe3a900fb6e4938be7ae194361dd363ce5d6fd0
KayKilb <115676011+KayKilb@users.noreply.github.com>
2024-10-08 22:10:51 -0500

    4. Outer Inner

---
+++ 0x01-dart-function/4-outer_inner.dart
@@ -0,0 +1,12 @@
+void outer(String name, String id) {
+  String inner() {
+    var names = name.split(' ');
+    return 'Hello Agent ${names[1][0]}.${names[0]} your id is $id';
+  }
+
+  print(inner());
+}
+
+void main() {
+  outer("Youssef Belhadj", "001");
+}
