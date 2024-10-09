82afab5cb83cfe87f6132513543eb727d3b36ea3
KayKilb <115676011+KayKilb@users.noreply.github.com>
2024-10-09 00:32:23 -0500

    Used Exception instead of throwing a generic string

--- 0x03-asynchronous/rick_and_morty.dart
+++ 0x03-asynchronous/rick_and_morty.dart
@@ -1,20 +1,32 @@
-import 'dart:convert';
 import 'package:http/http.dart' as http;
+import 'dart:convert';
 
 Future<void> printRmCharacters() async {
   try {
+    // Fetch data from the Rick and Morty API
     final response =
         await http.get(Uri.parse('https://rickandmortyapi.com/api/character'));
+
+    // Check if the request was successful
     if (response.statusCode == 200) {
+      // Decode the response body to extract character information
       Map<String, dynamic> data = jsonDecode(response.body);
-      List<dynamic> characters = data['results'];
-      for (var character in characters) {
-        print(character['name']);
+      List<dynamic>? characters = data['results'];
+
+      // Check if characters list is not null
+      if (characters != null) {
+        for (var character in characters) {
+          print(character['name']);
+        }
+      } else {
+        print('No characters found in the response.');
       }
     } else {
-      throw 'Failed to load characters';
+      throw Exception(
+          'Failed to load characters: Status Code ${response.statusCode}');
     }
   } catch (e) {
-    print('error caught: $e');
+    // Print the caught error
+    print('Error caught: $e');
   }
 }
